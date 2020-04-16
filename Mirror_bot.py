e9231078e6e8004bef6ffa7f7221d28e3b14c873
ayda@ayda <ayda.f.naderi@gmail.com>
2020-04-16 17:22:21 +0430

    edit mirror bot

--- Mirror_bot.py
+++ Mirror_bot.py
@@ -3,12 +3,12 @@ import time
 
 bot = telepot.Bot('TOKEN')
 
-def handle(msg):
+def mirror(msg):
     content_type,chat_type,chat_id = telepot.glance(msg)
     if content_type == 'text':
         bot.sendMessage(chat_id,"you said {}".format(msg["text"]))
 
-bot.message_loop(handle)
-print ('Listening ...')
+bot.message_loop(mirror)
+print ('waiting ...')
 while 1:
     time.sleep(10)
